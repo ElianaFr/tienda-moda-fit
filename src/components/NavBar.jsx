f980256d3a1eb238a6aaaea3bc662a61dc983c53
Laura Eliana Frosch <lauraelianafrosch@gmail.com>
2022-07-05 21:23:55 -0300

    logo navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,6 +4,7 @@ const NavBar = () => {
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
                 <span className= "pr-2">MODA FIT</span>
+                <LogoMf/>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
