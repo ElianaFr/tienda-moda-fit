6a65f8688b8c422b0b006ee1947fdf5a85c4f9b4
Laura Eliana Frosch <lauraelianafrosch@gmail.com>
2022-07-07 18:49:42 -0300

    componente promo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import NavBar from './components/Nav/NavBar';
 import ItemListContainer from './components/Items/ItemListContainer';
-
+import Promo from './components/Nav/Promo';
 
 
 function App() {
   
   return (
     <>
-    
+      <Promo promo={"HASTA 6 CUOTAS SIN INTERES, ENVIO GRATIS DESDE $10000.-"}/>
       <NavBar />
       <ItemListContainer saludo={"PROXIMAMENTE ARTICULOS"}/>
     </>
---
+++ src/components/Nav/Promo.jsx
@@ -0,0 +1,10 @@
+const Promo = (prop) => {
+    return (
+        <div class="bg-black mb-0">
+            <p class="txtPromo text-center mb-0"><i class="bi bi-truck truck me-2"></i>{prop.promo}</p>
+        </div>
+        
+    );
+}
+
+export default Promo;
