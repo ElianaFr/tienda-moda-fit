7c256535554c0407942b06d9b8d6819228755682
Laura Eliana Frosch <lauraelianafrosch@gmail.com>
2022-07-25 14:24:02 -0300

    arreglo carga de productos

--- src/components/Items/itemDetailContainer/ItemDetailContainer.jsx
+++ src/components/Items/itemDetailContainer/ItemDetailContainer.jsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from "react"
 import ItemDetail from "../itemDetail/ItemDetail";
 import stock from "../utils/stock.json"
 import { useParams } from "react-router-dom";
-
+import Spinner from "../spinner/spinner";
 
 function getItem(){
     return new Promise ((resolve)=>{
@@ -17,9 +17,12 @@ const ItemDetailContainer = () =>{
     const {id} = useParams();
     console.log("id seleccion",id)
 
+    const [loading,setLoading] = useState(false);
+    
     const [productos,setItems] = useState ({});
     
     useEffect(()=> {
+        setLoading(true);
         
         
         getItem().then((res) => {
@@ -27,10 +30,14 @@ const ItemDetailContainer = () =>{
             const itemSel = res.filter(articulo => articulo.id== id);
             console.log(itemSel)
             setItems(itemSel[0]);
+            setLoading(false);
             
         });
         
     },[]);
+
+    if(loading) return <Spinner />
+
     return(
         <>
         <ItemDetail productos= {productos}/>
--- src/components/Items/itemListContainer/ItemListContainer.jsx
+++ src/components/Items/itemListContainer/ItemListContainer.jsx
@@ -2,40 +2,42 @@ import React, {useState, useEffect} from "react"
 import ItemList from "../itemList/ItemList";
 import stock from "../utils/stock.json";
 import { useParams } from "react-router-dom";
+import Spinner from "../spinner/spinner";
+
 
 function getItem(){
     return new Promise ((resolve)=>{
         setTimeout(()=>{
             resolve(stock)
-        },1000);
+        },2000);
     });
 }
 
 const ItemListContainer = () =>{
     const {categoria} = useParams();
     console.log(categoria)
-
+    const [loading,setLoading] = useState(false);
     const [productos,setItems] = useState ([]);
 
     useEffect(()=> {
-            
+        setLoading(true)
         console.log("se vuelve a ejecutar")
         
         getItem().then((res) => {
             const itemProd = res;
-            console.log(itemProd)
+            console.log("respuesta de la promesa",itemProd)
         
             if(categoria){
-                setItems(itemProd.filter((articulos) => articulos.category == categoria))
+                setItems(itemProd.filter((elemento) => elemento.category == categoria))
                 console.log("array filtrado",itemProd)
             }else{
                 setItems(itemProd)
             }
-            
-        }
-        
-        );
+            setLoading(false)
+        });
     },[categoria]);
+    
+    if (loading) return <Spinner />;
     return(
         <>
         <ItemList productos = {productos}/>
