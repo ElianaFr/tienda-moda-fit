a68ac26e7058d6ec39c2133a15b649a364cd23ca
Eliana Frosch <lauraelianafrosch@gmail.com>
2023-02-06 17:08:54 -0300

    arreglo footer promociones

--- src/components/info/info.jsx
+++ src/components/info/info.jsx
@@ -3,28 +3,23 @@ import "../info/info.css"
 const Info = ()=>{
     return(
         <>
-            <div className="infoPie 	d-none d-lg-block">
+            <div className="infoPie d-sm-none d-md-block">
                 <ul className="infoList ">
                     <li className="list">
-                        <i className="bi bi-truck envio"></i>
+                        <i className="bi bi-truck "></i>
                         <span className="text">Realizamos envios a todo el país</span>
                     </li>
                     <li className="list">
-                        <i className="bi bi-credit-card envio"></i>
+                        <i className="bi bi-credit-card"></i>
                         <span className="text">3 y 6 cuotas sin interes</span>
                     </li>
                     <li className="list">
-                        <i className="bi bi-arrow-repeat envio"></i>
+                        <i className="bi bi-arrow-repeat "></i>
                         <span className="text">Primer cambio sin costo</span>
-
                     </li>
-                    <li>
-
-                    </li>
+                            
                 </ul>
             </div>
-            
-        
         </>
     );
 };
